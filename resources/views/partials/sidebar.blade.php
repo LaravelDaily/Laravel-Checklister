a329e3acb00ada6a575c73513bc6cb711b062c99
PovilasKorop <povilask007@gmail.com>
2021-04-27 07:48:59 +0300

    Small fix for sidebar checklist group to be editable

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -19,7 +19,7 @@
             <li class="c-sidebar-nav-title">{{ __('Manage Checklists') }}</li>
             @foreach (\App\Models\ChecklistGroup::with('checklists')->get() as $group)
                 <li class="c-sidebar-nav-item c-sidebar-nav-dropdown c-show">
-                    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle"
+                    <a class="c-sidebar-nav-link"
                        href="{{ route('admin.checklist_groups.edit', $group->id) }}">
                         <svg class="c-sidebar-nav-icon">
                             <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
