fb3c02897e7d845eb49ab044441472b4f9cd9b11
Nerijus <krekas@gmail.com>
2021-05-10 14:14:40 +0300

    Show uploaded file in ckeditor

--- resources/views/admin/tasks/edit.blade.php
+++ resources/views/admin/tasks/edit.blade.php
@@ -18,7 +18,7 @@
 
                         <form
                             action="{{ route('admin.checklists.tasks.update', [$checklist, $task]) }}"
-                            method="POST" id="task-form">
+                            method="POST" id="task-form" enctype="multipart/form-data">
                             @csrf
                             @method('PUT')
                             <div class="card-header">{{ __('Edit Task') }}</div>
@@ -51,63 +51,115 @@
 
 @section('scripts')
     <script>
-        function SimpleUploadAdapter(editor) {
-            editor.plugins.get('FileRepository').createUploadAdapter = function(loader) {
-                return {
-                    upload: function() {
-                        return loader.file
-                            .then(function (file) {
-                                return new Promise(function(resolve, reject) {
-                                    // Init request
-                                    var xhr = new XMLHttpRequest();
-                                    xhr.open('POST', '{{ route('admin.tasks.image_upload') }}', true);
-                                    xhr.setRequestHeader('x-csrf-token', '{{ csrf_token() }}');
-                                    xhr.setRequestHeader('Accept', 'application/json');
-                                    xhr.responseType = 'json';
-
-                                    // Init listeners
-                                    var genericErrorText = `Couldn't upload file: ${ file.name }.`;
-                                    xhr.addEventListener('error', function() { reject(genericErrorText) });
-                                    xhr.addEventListener('abort', function() { reject() });
-                                    xhr.addEventListener('load', function() {
-                                        var response = xhr.response;
-
-                                        if (!response || xhr.status !== 201) {
-                                            return reject(response && response.message ? `${genericErrorText}\n${xhr.status} ${response.message}` : `${genericErrorText}\n ${xhr.status} ${xhr.statusText}`);
-                                        }
-
-                                        document.getElementById("task-form").innerHTML += '<input type="hidden" name="ck-media[]" value="' + response.id + '">';
-
-                                        resolve({ default: response.url });
-                                    });
-
-                                    if (xhr.upload) {
-                                        xhr.upload.addEventListener('progress', function(e) {
-                                            if (e.lengthComputable) {
-                                                loader.uploadTotal = e.total;
-                                                loader.uploaded = e.loaded;
-                                            }
-                                        });
-                                    }
-
-                                    // Send request
-                                    var data = new FormData();
-                                    data.append('upload', file);
-                                    data.append('task_id', '{{ $task->id ?? 0 }}');
-                                    xhr.send(data);
-                                });
-                            })
-                    }
-                };
+        class MyUploadAdapter {
+            constructor( loader ) {
+                // The file loader instance to use during the upload.
+                this.loader = loader;
             }
+
+            // Starts the upload process.
+            upload() {
+                return this.loader.file
+                    .then( file => new Promise( ( resolve, reject ) => {
+                        this._initRequest();
+                        this._initListeners( resolve, reject, file );
+                        this._sendRequest( file );
+                    } ) );
+            }
+
+            // Aborts the upload process.
+            abort() {
+                if ( this.xhr ) {
+                    this.xhr.abort();
+                }
+            }
+
+            // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+            _initRequest() {
+                const xhr = this.xhr = new XMLHttpRequest();
+
+                // Note that your request may look different. It is up to you and your editor
+                // integration to choose the right communication channel. This example uses
+                // a POST request with JSON as a data structure but your configuration
+                // could be different.
+                xhr.open( 'POST', '{{ route('admin.tasks.image_upload') }}', true );
+                xhr.setRequestHeader('x-csrf-token', '{{ csrf_token() }}');
+                xhr.responseType = 'json';
+            }
+
+            // Initializes XMLHttpRequest listeners.
+            _initListeners( resolve, reject, file ) {
+                const xhr = this.xhr;
+                const loader = this.loader;
+                const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+
+                xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+                xhr.addEventListener( 'abort', () => reject() );
+                xhr.addEventListener( 'load', () => {
+                    const response = xhr.response;
+
+                    // This example assumes the XHR server's "response" object will come with
+                    // an "error" which has its own "message" that can be passed to reject()
+                    // in the upload promise.
+                    //
+                    // Your integration may handle upload errors in a different way so make sure
+                    // it is done properly. The reject() function must be called when the upload fails.
+                    if ( !response || response.error ) {
+                        return reject( response && response.error ? response.error.message : genericErrorText );
+                    }
+
+                    // If the upload is successful, resolve the upload promise with an object containing
+                    // at least the "default" URL, pointing to the image on the server.
+                    // This URL will be used to display the image in the content. Learn more in the
+                    // UploadAdapter#upload documentation.
+                    resolve( {
+                        default: response.url
+                    } );
+                } );
+
+                // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
+                // properties which are used e.g. to display the upload progress bar in the editor
+                // user interface.
+                if ( xhr.upload ) {
+                    xhr.upload.addEventListener( 'progress', evt => {
+                        if ( evt.lengthComputable ) {
+                            loader.uploadTotal = evt.total;
+                            loader.uploaded = evt.loaded;
+                        }
+                    } );
+                }
+            }
+
+            // Prepares the data and sends the request.
+            _sendRequest( file ) {
+                // Prepare the form data.
+                const data = new FormData();
+
+                data.append( 'upload', file );
+
+                // Important note: This is the right place to implement security mechanisms
+                // like authentication and CSRF protection. For instance, you can use
+                // XMLHttpRequest.setRequestHeader() to set the request headers containing
+                // the CSRF token generated earlier by your application.
+
+                // Send the request.
+                this.xhr.send( data );
+            }
+        }
+
+        function SimpleUploadAdapter( editor ) {
+            editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+                // Configure the URL to the upload script in your back-end here!
+                return new MyUploadAdapter( loader );
+            };
         }
 
         ClassicEditor
             .create( document.querySelector( '#task-textarea' ), {
-                extraPlugins: [SimpleUploadAdapter]
-            } )
+                extraPlugins: [ SimpleUploadAdapter ],
+            })
             .catch( error => {
                 console.error( error );
-            } );
+            });
     </script>
 @endsection
