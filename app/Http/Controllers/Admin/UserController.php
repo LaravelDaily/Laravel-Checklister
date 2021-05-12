e23e6c14d70fde7c22cc91d17635622689ca0ea0
Awan <m.awan.saifurrehman@gmail.com>
2021-05-12 17:03:29 +0500

    Admin change active/deactive user status

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
@@ -13,5 +13,13 @@ class UserController extends Controller
         $users = User::where('is_admin', 0)->latest()->paginate(50);
 
         return view('admin.users.index', compact('users'));
+    }
+    public function changeUserStatus($userId): RedirectResponse
+    {
+        $user = User::find($userId);
+        $user->is_active = $user->is_active ? '0' : '1';
+        $user->save();
+
+        return back()->with('message', $user->name . ' user status updated');
     }
 }
