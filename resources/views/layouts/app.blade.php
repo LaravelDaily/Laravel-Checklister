0962f136d2cc2f5ff7e17a79ef7e007e193ba50c
PovilasKorop <povilask007@gmail.com>
2021-06-03 09:38:52 +0300

    Forgot mobile menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,11 @@
     @include('partials.sidebar')
 <div class="c-wrapper c-fixed-components">
     <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
+        <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show">
+            <svg class="c-icon c-icon-lg">
+                <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-menu') }}"></use>
+            </svg>
+        </button>
         <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
             <svg class="c-icon c-icon-lg">
                 <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-menu') }}"></use>
